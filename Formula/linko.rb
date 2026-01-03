15233102f2fe1dd3513c14939e21a58eedb86168
monsterxx03 <xyj.asmy@gmail.com>
2026-01-03 15:00:50 +0800

    update

--- Formula/linko.rb
+++ Formula/linko.rb
@@ -5,51 +5,49 @@ class Linko < Formula
   # 直接从 GitHub Release 下载预编译的 arm64 二进制
   url "https://github.com/monsterxx03/linko/releases/download/v0.1.1/linko"
   version "0.1.1"
-  sha256 "4a65ad8e63f6bcb0500ca87683c5864434650f021b0a2ffeab314d683d11b587"
+  sha256 :binary
   license "MIT"
 
   def install
     bin.install "linko"
-    etc.install "config/linko.yaml" => "linko.yaml"
-    (etc/"linko.yaml").write DEFAULT_CONFIG
-      end
+    (etc/"linko.yaml").write default_config
+  end
 
-      def DEFAULT_CONFIG
-        <<~YAML
-          server:
-              listen_addr: 127.0.0.1:9890
-              log_level: info
-          dns:
-              listen_addr: 127.0.0.1:6363
-              domestic_dns:
-                  - 223.5.5.5
-                  - 114.114.114.114
-              foreign_dns:
-                  - 8.8.8.8
-                  - 1.1.1.1
-              cache_ttl: 5m0s
-              tcp_for_foreign: true
-          traffic:
-              enable_realtime: true
-              enable_history: true
-              update_interval: 1s
-              db_path: data/traffic.db
-          firewall:
-              enable_auto: true
-              redirect_dns: true
-              redirect_http: true
-              redirect_https: true
-          upstream:
-              enable: true
-              type: socks5
-              addr: 127.0.0.1:7891
-              username: ""
-              password: ""
-          admin:
-              enable: true
-              listen_addr: 0.0.0.0:9810
-        YAML
-      end
+  def default_config
+    <<~YAML
+      server:
+          listen_addr: 127.0.0.1:9890
+          log_level: info
+      dns:
+          listen_addr: 127.0.0.1:6363
+          domestic_dns:
+              - 223.5.5.5
+              - 114.114.114.114
+          foreign_dns:
+              - 8.8.8.8
+              - 1.1.1.1
+          cache_ttl: 5m0s
+          tcp_for_foreign: true
+      traffic:
+          enable_realtime: true
+          enable_history: true
+          update_interval: 1s
+          db_path: data/traffic.db
+      firewall:
+          enable_auto: true
+          redirect_dns: true
+          redirect_http: true
+          redirect_https: true
+      upstream:
+          enable: true
+          type: socks5
+          addr: 127.0.0.1:7891
+          username: ""
+          password: ""
+      admin:
+          enable: true
+          listen_addr: 0.0.0.0:9810
+    YAML
   end
 
   test do
