83c3f6fc94b96fe905a865a9156e2db8526213f3
monsterxx03 <xyj.asmy@gmail.com>
2026-01-03 14:46:05 +0800

    update

--- Formula/linko.rb
+++ Formula/linko.rb
@@ -3,9 +3,9 @@ class Linko < Formula
   homepage "https://github.com/monsterxx03/linko"
   
   # 直接从 GitHub Release 下载预编译的 arm64 二进制
-  url "https://github.com/monsterxx03/linko/releases/download/v0.1.0/linko"
-  version "0.1.0"
-  sha256 :binary
+  url "https://github.com/monsterxx03/linko/releases/download/v0.1.1/linko"
+  version "0.1.1"
+  sha256 "4a65ad8e63f6bcb0500ca87683c5864434650f021b0a2ffeab314d683d11b587"
   license "MIT"
 
   def install
