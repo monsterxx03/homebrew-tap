626b5f11dbb4f5fbabcb0d94da34a3f0c93be441
monsterxx03 <xyj.asmy@gmail.com>
2026-02-15 15:40:40 +0800

    fix

--- Formula/linko.rb
+++ Formula/linko.rb
@@ -6,15 +6,17 @@ class Linko < Formula
   if Hardware::CPU.arch == :arm64
     url "https://github.com/monsterxx03/linko/releases/download/v0.2.0/linko-darwin-arm64"
     sha256 "bcab428f42b20938d09800aa5bf61a90062e48f563ab0ea38d6a912d8bb7a080"
+    @filename = "linko-darwin-arm64"
   else
     url "https://github.com/monsterxx03/linko/releases/download/v0.2.0/linko-darwin-amd64"
     sha256 "afd5f34d78e2f9241370a5ca16bc175de00e71bb3382190824bc7696743849c8"
+    @filename = "linko-darwin-amd64"
   end
 
   bottle :unneeded
 
   def install
-    bin.install "linko-darwin-#{Hardware::CPU.arch == :arm64 ? "arm64" : "amd64"}" => "linko"
+    bin.install @filename => "linko"
   end
 
   test do
