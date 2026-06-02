792d947ca65606584d722104082ad8ae2a4f5134
monsterxx03 <xyj.asmy@gmail.com>
2026-06-02 22:40:39 +0800

    linko: bump version to v0.5.5
    Co-authored-by: deepseek-v4-flash <deepseek-v4-flash@tachi>

--- Formula/linko.rb
+++ Formula/linko.rb
@@ -1,14 +1,14 @@
 class Linko < Formula
   desc "A CLI tool for something"
   homepage "https://github.com/monsterxx03/linko"
-  version "v0.5.4"
+  version "v0.5.5"
 
   if Hardware::CPU.arch == :arm64
-    url "https://github.com/monsterxx03/linko/releases/download/v0.5.4/linko-darwin-arm64"
-    sha256 "1e1bff40aa6099cd663c25d770b01ad2e945454c8ce29ee72d134be53cb239c7"
+    url "https://github.com/monsterxx03/linko/releases/download/v0.5.5/linko-darwin-arm64"
+    sha256 "ee64dea479e5022794c22a09f9d8b77dbf74f5740262fb6f6f31291473660732"
   else
-    url "https://github.com/monsterxx03/linko/releases/download/v0.5.4/linko-darwin-amd64"
-    sha256 "9ad173750d98318add2f0997c8c61ef203cef908cd349f09c8344035eee5a78e"
+    url "https://github.com/monsterxx03/linko/releases/download/v0.5.5/linko-darwin-amd64"
+    sha256 "0188a0d6a0a0e8c3aa65c115267d781800184dc10ea03de1100932ab763aa687"
   end
 
   def install
